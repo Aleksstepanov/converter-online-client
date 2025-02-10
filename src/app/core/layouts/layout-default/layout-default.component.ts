b0e5d0d7c481863f1c86fab01e4c31ba10e1ac3a
Aleksey <aleksey@alexey.stepanov>
2025-02-10 09:35:14 +0300

    add providers routes, add layouts

---
+++ src/app/core/layouts/layout-default/layout-default.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+
+@Component({
+  selector: 'app-layout-default',
+  imports: [
+    RouterOutlet
+  ],
+  templateUrl: './layout-default.component.html',
+  styleUrl: './layout-default.component.css'
+})
+export class LayoutDefaultComponent {
+
+}
---
+++ src/app/providers/router/app.routes.ts
@@ -0,0 +1,28 @@
+import { Routes } from '@angular/router';
+import {LoginComponent} from '../../core/pages/login/login.component'
+import {HomeComponent} from '../../core/pages/home/home.component'
+import {LayoutDefaultComponent} from '../../core/layouts/layout-default/layout-default.component'
+import {LayoutPublicComponent} from '../../core/layouts/layout-public/layout-public.component'
+
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: LayoutPublicComponent,
+    children: [
+      {
+        path: '',
+        component: LoginComponent
+      }
+    ]
+  },
+  {
+    path: 'home',
+    component: LayoutDefaultComponent,
+    children: [
+      {
+        path: '',
+        component: HomeComponent
+      }
+    ]
+  }
+];
