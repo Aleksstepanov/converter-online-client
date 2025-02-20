179dcbc38633df17b11b8b5103e97eaf8d43280f
Aleksey <aleksey@alexey.stepanov>
2025-02-20 10:36:00 +0300

    fix struct

--- src/app/core/router/app.routes.ts
+++ src/app/core/router/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from '../../core/pages/login/login.component';
-import { HomeComponent } from '../../core/pages/home/home.component';
-import { LayoutDefaultComponent } from '../../core/layouts/layout-default/layout-default.component';
-import { LayoutPublicComponent } from '../../core/layouts/layout-public/layout-public.component';
+import { LoginComponent } from '@pages/login/login.component';
+import { HomeComponent } from '@pages/home/home.component';
+import { LayoutDefaultComponent } from '@core/layouts/layout-default/layout-default.component';
+import { LayoutPublicComponent } from '@core/layouts/layout-public/layout-public.component';
 
 export const routes: Routes = [
   {
